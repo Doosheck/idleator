94451e7c02480898d67e35f017ec9605d9e8a224
Doosheck <michal.adam.kasperek@gmail.com>
2024-04-04 17:59:05 +0200

    Add mouse movement and click functionality to idleator.py

--- idleator.py
+++ idleator.py
@@ -6,13 +6,23 @@ import time
 import pyautogui
 
 def main():
+    count = 0
     while True:
         # Check if the mouse was moved in the last 240 seconds
         if pyautogui.onScreen(0, 0):
-            pyautogui.moveRel(1, 1)
-            pyautogui.moveRel(-1, -1)
+            # Move the mouse in a circle
+            pyautogui.move(20, 0, duration=0.25)
+            pyautogui.move(0, 20, duration=0.25)
+            pyautogui.move(-20, 0, duration=0.25)
+            pyautogui.move(0, -20, duration=0.25)
+            # Move to the top left corner of the screen and click
+            pyautogui.moveTo(100, 100, duration=0.25)
+            pyautogui.click()
+            #Go back to the center of the screen
+            pyautogui.moveTo(960, 540, duration=0.25)
             time.sleep(1)
-            print('Mouse moved')
+            count += 1
+            print('Mouse moved, count:', count)
         else:
             time.sleep(240)
 
