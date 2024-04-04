97ec25e4fb5f766d7745efd13d0ec636c9a8185f
Michal Kasperek <michal.kasperek@siemens.com>
2024-04-04 15:18:39 +0200

    Add idleator.py script for preventing computer sleep

---
+++ idleator.py
@@ -0,0 +1,19 @@
+'''This script is the main script for the Idleator project.
+Its purpose is to move the mouse a little bit every few seconds to prevent the computer from going to sleep.
+This is useful for when you are watching a video or reading a long article and you don't want the computer to go to sleep. '''
+
+import time
+import pyautogui
+
+def main():
+    while True:
+        # Check if the mouse was moved in the last 240 seconds
+        if pyautogui.onScreen(0, 0):
+            # Move the mouse a little bit
+            pyautogui.moveRel(1, 1)
+            pyautogui.moveRel(-1, -1)
+            time.sleep(240)
+
+
+if __name__ == '__main__':
+    main()
